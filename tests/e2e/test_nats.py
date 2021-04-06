14aaaf8e179a1d248cef7199c0c9a784a68d20e4
juan.macias <Juan.Macias@sa.catapult.org.uk>
2021-04-06 19:04:56 +0100

    Add new stac item test

--- tests/e2e/test_nats.py
+++ tests/e2e/test_nats.py
@@ -1,5 +1,6 @@
 import asyncio
 import os
+import shutil
 from pathlib import Path
 
 from nats.aio.client import Client as NATS
@@ -16,6 +17,23 @@ def initialise_s3_bucket(sensor_key, s3_resource, bucket_name):
             Filename=str(file),
             Key=f"{sensor_key}{file.parent.stem}/{file.name}"
         )
+
+
+def add_stac_s3(sensor_name, s3_resource, bucket_name):
+    s3_resource.Bucket(bucket_name).upload_file(
+        Filename='tests/output/catalog.json',
+        Key='stac_catalogs/cs_stac/catalog.json'
+    )
+    s3_resource.Bucket(bucket_name).upload_file(
+        Filename=f'tests/output/{sensor_name}/collection.json',
+        Key=f'stac_catalogs/cs_stac/{sensor_name}/collection.json'
+    )
+    for file in Path(f'tests/output/{sensor_name}').glob('**/*.json'):
+        if 'collection' not in file.name:
+            s3_resource.Bucket(bucket_name).upload_file(
+                Filename=str(file),
+                Key=f"stac_catalogs/cs_stac/{sensor_name}/{file.stem}/{file.name}"
+            )
 
 
 async def client(nc, sensor_key):
@@ -65,4 +83,52 @@ def test_new_stac_collection():
                          'LT05_L1TP_075073_19911225/LT05_L1TP_075073_19911225.json']
 
     finally:
+        os.environ.pop("TEST_ENV")
+
+
+async def client_item(nc, acquisition_key):
+    future = asyncio.Future()
+
+    async def message_handler(msg):
+        data = msg.data.decode()
+        future.set_result(data)
+
+    await nc.subscribe("stac_indexer.*", cb=message_handler)
+    await nc.publish("stac_creator.item", acquisition_key.encode())
+    return await asyncio.wait_for(future, 1)
+
+
+@mock_s3
+def test_new_stac_item():
+    sensor_name = 'landsat_5'
+    sensor_key = f'common_sensing/fiji/{sensor_name}/'
+    acquisition_key = f'{sensor_key}LT05_L1TP_075073_19920125/'
+    bucket = 'public-eo-data'
+    try:
+        os.environ["TEST_ENV"] = "Yes"
+
+        s3 = S3(key=None, secret=None, s3_endpoint=None, region_name='us-east-1')
+
+        shutil.copytree(f'tests/data/test_add_stac_item/{acquisition_key}',
+                        f'tests/data/{acquisition_key}')
+
+        initialise_s3_bucket(sensor_key, s3.s3_resource, bucket)
+        add_stac_s3(sensor_name, s3.s3_resource, bucket)
+
+        repo = repository.S3Repository(s3)
+
+        event_loop = asyncio.get_event_loop()
+        nats_client = NATS()
+
+        event_loop.run_until_complete(run(nats_client, repo, event_loop))
+        item_key = event_loop.run_until_complete(client_item(nats_client, acquisition_key))
+        event_loop.run_until_complete(close_nats(nats_client, event_loop))
+
+        event_loop.run_forever()
+        event_loop.close()
+
+        assert item_key == 'stac_catalogs/cs_stac/landsat_5/LT05_L1TP_075073_19920125/LT05_L1TP_075073_19920125.json'
+
+    finally:
+        shutil.rmtree(f'tests/data/{acquisition_key}')
         os.environ.pop("TEST_ENV")
